918d82bb6d2aca5bb1576671d4b1c65f112f2f2b
James Kirk <JamesKirk95@hotmail.com>
2020-05-27 13:30:08 +0100

    Update products/bash/release-products.sh
    Co-authored-by: Daniel Pink <64848511+DanRoseus@users.noreply.github.com>

--- products/bash/release-products.sh
+++ products/bash/release-products.sh
@@ -43,7 +43,7 @@ if [[ -z "${cp_console}" ]]; then
     exit 2
 fi
 if [[ -z "${cp_products}" ]]; then
-    cp_products="apic ace assetrepo eventstreams tracingmq"
+    cp_products="apic ace assetrepo eventstreams tracing mq"
 fi
 if [[ -z "${cp_password}" ]]; then
     read -p "Password (${cp_username}): " -s -r cp_password
