68c053a83a2c5a161981a2220695461e03fa38aa
James Kirk <jameskirk95@hotmail.com>
2020-06-19 15:03:51 +0100

    initial push

---
+++ DrivewayDentDeletion/continuous-load.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+#******************************************************************************
+# Licensed Materials - Property of IBM
+# (c) Copyright IBM Corporation 2020. All Rights Reserved.
+#
+# Note to U.S. Government Users Restricted Rights:
+# Use, duplication or disclosure restricted by GSA ADP Schedule
+# Contract with IBM Corp.
+#******************************************************************************
+#
+# INSTRUCTIONS
+# ------------
+#
+# 1. Run the script, passing the Cloud Pak console address as argument:
+#       ./release-products.sh icp-console.<your-cluster-domain>
+
--- products/bash/release-products.sh
+++ products/bash/release-products.sh
@@ -1,4 +1,12 @@
 #!/bin/bash
+#******************************************************************************
+# Licensed Materials - Property of IBM
+# (c) Copyright IBM Corporation 2020. All Rights Reserved.
+#
+# Note to U.S. Government Users Restricted Rights:
+# Use, duplication or disclosure restricted by GSA ADP Schedule
+# Contract with IBM Corp.
+#******************************************************************************
 #
 # INSTRUCTIONS
 # ------------
