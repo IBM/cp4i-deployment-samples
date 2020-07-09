2d995aea8154a06a939700fef6c8c9ff76a9aee7
Daniel Pink <daniel.pink@uk.ibm.com>
2020-07-09 10:34:51 +0100

    Initial version of script to release APIC

---
+++ products/bash/release-apic.sh
@@ -0,0 +1,56 @@
+#!/bin/bash
+#******************************************************************************
+# Licensed Materials - Property of IBM
+# (c) Copyright IBM Corporation 2020. All Rights Reserved.
+#
+# Note to U.S. Government Users Restricted Rights:
+# Use, duplication or disclosure restricted by GSA ADP Schedule
+# Contract with IBM Corp.
+#******************************************************************************
+
+#******************************************************************************
+# PREREQUISITES:
+#   - Logged into cluster on the OC CLI (https://docs.openshift.com/container-platform/4.4/cli_reference/openshift_cli/getting-started-cli.html)
+#
+# PARAMETERS:
+#   -n : <namespace> (string), Defaults to "cp4i"
+#   -r : <release-name> (string), Defaults to "demo"
+#
+# USAGE:
+#   With defaults values
+#     ./release-apic.sh
+#
+#   Overriding the namespace and release-name
+#     ./release-apic -n cp4i-prod -r prod
+
+function usage {
+    echo "Usage: $0 -n <namespace> -r <release-name>"
+}
+
+namespace="cp4i"
+release_name="demo"
+
+while getopts "n:r:" opt; do
+  case ${opt} in
+    n ) namespace="$OPTARG"
+      ;;
+    r ) release_name="$OPTARG"
+      ;;
+    \? ) usage; exit
+      ;;
+  esac
+done
+
+cat << EOF | oc apply -f -
+apiVersion: apiconnect.ibm.com/v1beta1
+kind: APIConnectCluster
+metadata:
+  name: ${release_name}
+  namespace: ${namespace}
+spec:
+  appVersion: 10.0.0.0
+  license:
+    accept: true
+    use: nonproduction
+  profile: n3xc4.m16
+EOF
