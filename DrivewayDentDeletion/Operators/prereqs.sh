cc6a73445bd889de4143e8e0b0c95d5bd110dbcc
Thomas Li <thomas.li6@ibm.com>
2020-07-23 15:06:21 +0100

    New deploy scripts

--- DrivewayDentDeletion/Operators/prereqs.sh
+++ DrivewayDentDeletion/Operators/prereqs.sh
@@ -71,7 +71,7 @@ ls ${PWD}/DefaultPolicies
 zip -r DefaultPolicies/policyproject.zip DefaultPolicies/
 
 echo "INFO: encoding the policy project"
-temp=$(base64 --wrap=0 ${PWD}/DefaultPolicies/policyproject.zip)
+temp=$(base64 ${PWD}/DefaultPolicies/policyproject.zip)
 
 configyaml="\
 apiVersion: appconnect.ibm.com/v1beta1
@@ -87,3 +87,23 @@ echo "${configyaml}" > ${PWD}/tmp/policy-project-config.yaml
 echo "INFO: Output -> policy-project-config.yaml"
 cat ${PWD}/tmp/policy-project-config.yaml
 oc apply -f ${PWD}/tmp/policy-project-config.yaml
+
+echo "Waiting for postgres to be ready"
+oc wait -n postgres --for=condition=available deploymentconfig --timeout=20m postgresql
+
+echo "Creating quotes table in postgres samepledb"
+oc exec -n postgres -it $(oc get pod -n postgres -l name=postgresql -o jsonpath='{.items[].metadata.name}') \
+  -- psql -U admin -d sampledb -c \
+'CREATE TABLE QUOTES (
+  QuoteID SERIAL PRIMARY KEY NOT NULL,
+  Name VARCHAR(100),
+  EMail VARCHAR(100),
+  Address VARCHAR(100),
+  USState VARCHAR(100),
+  LicensePlate VARCHAR(100),
+  ACMECost INTEGER,
+  ACMEDate DATE,
+  BernieCost INTEGER,
+  BernieDate DATE,
+  ChrisCost INTEGER,
+  ChrisDate DATE);'
