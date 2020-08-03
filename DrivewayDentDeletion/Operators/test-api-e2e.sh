0e28df79b406f8c7b63bd5c8326b364e0712fa97
Kinshuk Bhardwaj <kinshuk.bhardwaj@ibm.com>
2020-08-03 17:01:58 +0100

    Update runAfter for test

--- DrivewayDentDeletion/Operators/test-api-e2e.sh
+++ DrivewayDentDeletion/Operators/test-api-e2e.sh
@@ -12,14 +12,14 @@
 #   - Logged into cluster on the OC CLI (https://docs.openshift.com/container-platform/4.4/cli_reference/openshift_cli/getting-started-cli.html)
 #
 # PARAMETERS:
-#   -i : <condensed_info> (true/false), whether to show the full post response or a condensed version - DEFAULT: false
+#   -c : <condensed_info> (true/false), whether to show the full post response or a condensed version - DEFAULT: false
 #   -n : <namespace> (string), Defaults to "cp4i"
 #
 #   With defaults values
 #     ./test-api-e2e.sh -n <namesapce>
 
 function usage {
-    echo "Usage: $0 -n <namespace> -t <imageTag> -i"
+    echo "Usage: $0 -n <namespace> -t <imageTag> -c"
 }
 
 namespace="cp4i"
@@ -35,7 +35,7 @@ while getopts "n:ti" opt; do
       ;;
     t ) imageTag="$OPTARG"
       ;;
-    i)
+    c)
       condensed_info=true
       ;;
     \? ) usage; exit
@@ -54,12 +54,14 @@ while [ "$numberOfReadyRunningDemoPods" != "9" ]; do
   echo "Waiting upto 30 minutes for all integration demo pods to be in Ready and Running state. Waited ${time} minute(s)."
   time=$((time + 1))
   numberOfReadyRunningDemoPods=$(oc get pods -n ${namespace} | grep -E "int-srv-${imageTag}|mq-ddd-qm-latest" | grep -v pipelinerun | grep 1/1 | awk '{print $3}' | wc -l | xargs)
+  echo "INFO: The integration server pods are:"
+  oc get pods | grep -E "int-srv-${imageTag}|mq-ddd-qm-latest"
   sleep 60
 done
 
 echo "All demo pods are up, ready and in running state, going ahead with testing API..."
-echo "INFO: The integration server pods are:"
-oc get pods | grep int-srv-${imageTag}
+echo "INFO: The integration server and mq pods are:"
+oc get pods | grep -E "int-srv-${imageTag}|mq-ddd-qm-latest"
 
 
 echo -e "\nPOST request..."
@@ -74,7 +76,7 @@ if [ "$post_response_code" == "200" ]; then
   # The usage of sed here is to prevent an error caused between the -w flag of curl and jq not interacting well
   quote_id=$(echo "$post_response" | jq '.' | sed $os_sed_flag '$ d' | jq '.QuoteID')
 
-  echo -e "SUCCESS - POSTed with response code: ${post_response_code}, QuoteID: ${quote_id}, and Response Body:\n"
+  echo -e "INFO: SUCCESS - POSTed with response code: ${post_response_code}, QuoteID: ${quote_id}, and Response Body:\n"
   if [ "$condensed_info" = true ]; then
     # The usage of sed here is to prevent an error caused between the -w flag of curl and jq not interacting well
     echo ${post_response} | jq '.' | sed $os_sed_flag '$ d' | jq '{ QuoteID: .QuoteID, Versions: .Versions }'
@@ -88,7 +90,7 @@ if [ "$post_response_code" == "200" ]; then
   get_response_code=$(echo "${get_response##* }")
 
   if [ "$get_response_code" == "200" ]; then
-    echo -e "SUCCESS - GETed with response code: ${get_response_code}, and Response Body:\n"
+    echo -e "INFO: SUCCESS - GETed with response code: ${get_response_code}, and Response Body:\n"
 
     if [ "$condensed_info" = true ]; then
       # The usage of sed here is to prevent an error caused between the -w flag of curl and jq not interacting well
@@ -97,13 +99,13 @@ if [ "$post_response_code" == "200" ]; then
       echo ${get_response} | jq '.' | sed $os_sed_flag '$ d'
     fi
   else
-    echo "FAILED - Error code: ${get_response_code}"
+    echo "ERROR: FAILED - Error code: ${get_response_code}"
   fi
 
-  echo -e "\nDeleting row from database..."
+  echo -e "INFO: \nDeleting row from database..."
   oc exec -n postgres -it $(oc get pod -n postgres -l name=postgresql -o jsonpath='{.items[].metadata.name}') \
     -- psql -U admin -d sampledb -c \
     "DELETE FROM quotes WHERE quotes.quoteid = ${quote_id};"
 else
-  echo "FAILED - Error code: ${post_response_code}" # Failure catch during POST
+  echo "ERROR: FAILED - Error code: ${post_response_code}" # Failure catch during POST
 fi
