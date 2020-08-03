8348c5cad17ea24bb6a85c23a4f0e5229db226f2
Kinshuk Bhardwaj <kinshuk.bhardwaj@ibm.com>
2020-08-03 17:01:58 +0100

    Update runAfter for test

--- DrivewayDentDeletion/Operators/test-api-e2e.sh
+++ DrivewayDentDeletion/Operators/test-api-e2e.sh
@@ -12,6 +12,7 @@
 #   - Logged into cluster on the OC CLI (https://docs.openshift.com/container-platform/4.4/cli_reference/openshift_cli/getting-started-cli.html)
 #
 # PARAMETERS:
+#   -c : <condensed_info> (true/false), whether to show the full post response or a condensed version - DEFAULT: false
 #   -n : <namespace> (string), Defaults to "cp4i"
 #   -t : <imageTag> (string), Default is empty
 #
@@ -19,7 +20,7 @@
 #     ./test-api-e2e.sh -n <namesapce>
 
 function usage {
-    echo "Usage: $0 -n <namespace>"
+    echo "Usage: $0 -n <namespace> -t <imageTag> -c"
 }
 
 namespace="cp4i"
@@ -32,6 +33,11 @@ fi
 while getopts "n:t:c" opt; do
   case ${opt} in
     n ) namespace="$OPTARG"
+      ;;
+    t ) imageTag="$OPTARG"
+      ;;
+    c)
+      condensed_info=true
       ;;
     \? ) usage; exit
       ;;
@@ -65,9 +71,17 @@ if [[ "$jqInstalled" == "false" ]]; then
     /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
     brew install jq
   fi
-fi
+  echo "Waiting upto 30 minutes for all integration demo pods to be in Ready and Running state. Waited ${time} minute(s)."
+  time=$((time + 1))
+  numberOfReadyRunningDemoPods=$(oc get pods -n ${namespace} | grep -E "int-srv-${imageTag}|mq-ddd-qm-latest" | grep -v pipelinerun | grep 1/1 | awk '{print $3}' | wc -l | xargs)
+  echo "INFO: The integration server pods are:"
+  oc get pods | grep -E "int-srv-${imageTag}|mq-ddd-qm-latest"
+  sleep 60
+done
 
-echo -e "\nINFO: Installed JQ version is $($JQ --version)"
+echo "All demo pods are up, ready and in running state, going ahead with testing API..."
+echo "INFO: The integration server and mq pods are:"
+oc get pods | grep -E "int-srv-${imageTag}|mq-ddd-qm-latest"
 
 echo -e "\n----------------------------------------------------------------------------------------------------------------------------------------------------------\n"
 
@@ -91,9 +105,13 @@ if [ "$post_response_code" == "200" ]; then
   # The usage of sed here is to prevent an error caused between the -w flag of curl and jq not interacting well
   quote_id=$(echo "$post_response" | $JQ '.' | sed $os_sed_flag '$ d' | $JQ '.QuoteID')
 
-  echo -e "INFO: SUCCESS - POST with response code: ${post_response_code}, QuoteID: ${quote_id}, and Response Body:\n"
-  # The usage of sed here is to prevent an error caused between the -w flag of curl and jq not interacting well
-  echo ${post_response} | $JQ '.' | sed $os_sed_flag '$ d'
+  echo -e "INFO: SUCCESS - POSTed with response code: ${post_response_code}, QuoteID: ${quote_id}, and Response Body:\n"
+  if [ "$condensed_info" = true ]; then
+    # The usage of sed here is to prevent an error caused between the -w flag of curl and jq not interacting well
+    echo ${post_response} | jq '.' | sed $os_sed_flag '$ d' | jq '{ QuoteID: .QuoteID, Versions: .Versions }'
+  else
+    echo ${post_response} | jq '.' | sed $os_sed_flag '$ d'
+  fi
 
   echo -e "\n----------------------------------------------------------------------------------------------------------------------------------------------------------"
 
@@ -103,29 +121,19 @@ if [ "$post_response_code" == "200" ]; then
   get_response_code=$(echo "${get_response##* }")
 
   if [ "$get_response_code" == "200" ]; then
-    echo -e "INFO: SUCCESS - GET with response code: ${get_response_code}, and Response Body:\n"
-    # The usage of sed here is to prevent an error caused between the -w flag of curl and jq not interacting well
-    echo ${get_response} | $JQ '.' | sed $os_sed_flag '$ d'
+    echo -e "INFO: SUCCESS - GETed with response code: ${get_response_code}, and Response Body:\n"
 
-    echo -e "\n----------------------------------------------------------------------------------------------------------------------------------------------------------"
-
-    #  ------- Get row to confirm post -------
-    echo -e "\nINFO: Select and print the row as user '${USERNAME}' from database '${DB_NAME}' with id '$quote_id' to confirm deletion..."
-    oc exec -n postgres -it $(oc get pod -n postgres -l name=postgresql -o jsonpath='{.items[].metadata.name}') \
-      -- psql -U ${USERNAME} -d ${DB_NAME} -c \
-      "SELECT * FROM quotes WHERE quotes.quoteid=${quote_id};"
-
-    echo -e "\n----------------------------------------------------------------------------------------------------------------------------------------------------------\n"
+    if [ "$condensed_info" = true ]; then
+      # The usage of sed here is to prevent an error caused between the -w flag of curl and jq not interacting well
+      echo ${get_response} | jq '.' | sed $os_sed_flag '$ d' | jq '.[0] | { QuoteID: .QuoteID, Email: .Email }'
+    else
+      echo ${get_response} | jq '.' | sed $os_sed_flag '$ d'
+    fi
   else
     echo "ERROR: FAILED - Error code: ${get_response_code}"
-    echo -e "\n----------------------------------------------------------------------------------------------------------------------------------------------------------\n"
-    exit 1
   fi
 
-  echo -e "\n----------------------------------------------------------------------------------------------------------------------------------------------------------"
-  # ------- Delete from the database -------
-  echo -e "\nINFO: Deleting row from database '${DB_NAME}' as user '${USERNAME}'..."
-  echo "INFO: Deleting the row with quote id $quote_id from the database"
+  echo -e "INFO: \nDeleting row from database..."
   oc exec -n postgres -it $(oc get pod -n postgres -l name=postgresql -o jsonpath='{.items[].metadata.name}') \
     -- psql -U ${USERNAME} -d ${DB_NAME} -c \
     "DELETE FROM quotes WHERE quotes.quoteid=${quote_id};"
@@ -139,8 +147,5 @@ if [ "$post_response_code" == "200" ]; then
     "SELECT * FROM quotes WHERE quotes.quoteid=${quote_id};"
 
 else
-  # Failure catch during POST
-  echo "ERROR: Post request failed - Error code: ${post_response_code}"
-  exit 1
+  echo "ERROR: FAILED - Error code: ${post_response_code}" # Failure catch during POST
 fi
-echo -e "----------------------------------------------------------------------------------------------------------------------------------------------------------"
