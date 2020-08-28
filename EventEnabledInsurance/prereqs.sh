1b892b1740b124f91f6a2d7f7168ff1240830746
Kinshuk Bhardwaj <kkb1993.kinshuk@gmail.com>
2020-08-28 15:11:36 +0100

    Dummy Prereqs

---
+++ EventEnabledInsurance/prereqs.sh
@@ -0,0 +1,53 @@
+#!/bin/bash
+#******************************************************************************
+# Licensed Materials - Property of IBM
+# (c) Copyright IBM Corporation 2019. All Rights Reserved.
+#
+# Note to U.S. Government Users Restricted Rights:
+# Use, duplication or disclosure restricted by GSA ADP Schedule
+# Contract with IBM Corp.
+#******************************************************************************
+
+# PREREQUISITES:
+#   - Logged into cluster on the OC CLI (https://docs.openshift.com/container-platform/4.4/cli_reference/openshift_cli/getting-started-cli.html)
+#
+# PARAMETERS:
+#   -n : <namespace> (string), Defaults to 'cp4i'
+#
+#   With defaults values
+#     ./prereqs.sh
+#
+#   With overridden values
+#     ./prereqs.sh -n <namespace>
+
+function usage() {
+  echo "Usage: $0 -n <namespace>"
+  exit 1
+}
+
+namespace="cp4i"
+tick="\xE2\x9C\x85"
+cross="\xE2\x9D\x8C"
+all_done="\xF0\x9F\x92\xAF"
+SUFFIX="eei"
+POSTGRES_NAMESPACE="postgres"
+ACE_CONFIGURATION_NAME="ace-policyproject-$SUFFIX"
+
+while getopts "n:" opt; do
+  case ${opt} in
+  n)
+    namespace="$OPTARG"
+    ;;
+  \?)
+    usage
+    exit
+    ;;
+  esac
+done
+
+CURRENT_DIR=$(dirname $0)
+echo "INFO: Current directory: '$CURRENT_DIR'"
+echo "INFO: Namespace: '$namespace'"
+echo "INFO: Suffix for the postgres is: '$SUFFIX'"
+
+echo -e "\n----------------------------------------------------------------------------------------------------------------------------------------------------------\n"
