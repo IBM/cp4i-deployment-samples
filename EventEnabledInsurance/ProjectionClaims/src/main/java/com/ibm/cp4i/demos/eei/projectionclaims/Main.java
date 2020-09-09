f2d0da7983a9cc1e77bd99fe55012117a39af3f8
Kinshuk Bhardwaj <kkb1993.kinshuk@gmail.com>
2020-09-09 12:04:29 +0100

    Add code for simple web server get

--- EventEnabledInsurance/ProjectionClaims/src/main/java/com/ibm/cp4i/demos/eei/projectionclaims/Main.java
+++ EventEnabledInsurance/ProjectionClaims/src/main/java/com/ibm/cp4i/demos/eei/projectionclaims/Main.java
@@ -1,9 +1,23 @@
 package com.ibm.cp4i.demos.eei.projectionclaims;
 
-import static java.lang.System.out;
+import com.sun.net.httpserver.HttpServer;
+
+import java.io.IOException;
+import java.net.InetSocketAddress;
 
 public class Main {
-    public static void main (String[] args) {
-        out.println("Hello world");
+    static final int PORT = 9000;
+    public static void main(String[] args) throws IOException {
+        System.out.println("Hello world");
+        // Construct an HTTP server object
+        HttpServer server = HttpServer.create(new InetSocketAddress("localhost", Main.PORT), 0);
+        System.out.println("server started at " + Main.PORT);
+        // Implement HTTP handler to process GET/POST requests and generate responses
+        server.createContext("/", new RootHandler());
+        // HTTP handler objects to the HTTP server object
+        server.createContext("/echoHeader", new EchoHeaderHandler());
+        server.createContext("/echoGet", new EchoGetHandler());
+        server.setExecutor(null);
+        server.start();
     }
 }
