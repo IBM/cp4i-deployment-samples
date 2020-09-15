a430b249cc46ae49dc7cfab24afbbca1dff9bad9
Kinshuk Bhardwaj <kkb1993.kinshuk@gmail.com>
2020-09-15 14:30:25 +0100

    Get different data, handle favicon request, display table

--- EventEnabledInsurance/ProjectionClaims/src/main/java/com/ibm/cp4i/demos/eei/projectionclaims/MyHttpHandler.java
+++ EventEnabledInsurance/ProjectionClaims/src/main/java/com/ibm/cp4i/demos/eei/projectionclaims/MyHttpHandler.java
@@ -3,7 +3,6 @@ package com.ibm.cp4i.demos.eei.projectionclaims;
 import com.fasterxml.jackson.databind.JsonNode;
 import com.sun.net.httpserver.HttpExchange;
 import com.sun.net.httpserver.HttpHandler;
-
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
@@ -51,38 +50,66 @@ public class MyHttpHandler implements HttpHandler {
         // get all table data
         if (requestParamValue.equalsIgnoreCase("all")) {
             System.out.println("Requested for all data");
+            in = new BufferedReader(new FileReader("./src/main/resources/index.html"));
             try {
                 JsonNode table = this.monitor.getTable();
                 System.out.println("==============================");
-                System.out.println(table.toPrettyString());
+                while ((str = in.readLine()) != null) {
+                    if (str.equalsIgnoreCase("<h2>We're starting here!</h2>") && (table == null)) {
+                        contentBuilder.append("<h2>Searched for the quote id: ").append(requestParamValue).append(", but no record found.").append("</h2>");
+                    } else if (str.equalsIgnoreCase("<h2>We're starting here!</h2>") && (table != null)) {
+                        System.out.println("Total data found: " + table.size());
+                        contentBuilder.append("<h2>Searched for all table data and found ").append(table.size()).append(" records:").append("</h2>");
+                        contentBuilder.append("<h3>").append(table.toPrettyString()).append("</h3>");
+                    } else {
+                        contentBuilder.append(str);
+                    }
+                }
             } catch (Throwable exception) {
                 exception.printStackTrace();
-            }
-            // TODO: EDIT/REMOVE
-            in = new BufferedReader(new FileReader("./src/main/resources/index.html"));
-            while ((str = in.readLine()) != null) {
-                contentBuilder.append(str);
             }
         }
         // get a particular quote id
         else if (!requestParamValue.isEmpty()) {
             System.out.println("Requested for a particular quote id " + requestParamValue);
+            in = new BufferedReader(new FileReader("./src/main/resources/index.html"));
             try {
-                System.out.println("----------------------------------");
+                System.out.println("==============================");
                 int id = Integer.parseInt(requestParamValue);
                 JsonNode row = this.monitor.getRow(id);
-                System.out.println(row.toPrettyString());
+                while ((str = in.readLine()) != null) {
+                    if (str.equalsIgnoreCase("<h2>We're starting here!</h2>") && (row == null)) {
+                        contentBuilder.append("<h2>Searched for the quote id: ").append(requestParamValue).append(", but no record found.").append("</h2>");
+                    } else if (str.equalsIgnoreCase("<h2>We're starting here!</h2>") && (row != null)) {
+                        System.out.println(row.toPrettyString());
+//                        contentBuilder.append("<h2>Searched for the quote id: ").append(requestParamValue).append(", found the following data.").append("</h2>");
+                        contentBuilder.append(
+                                "<table style=\"width:100%\">" +
+                                "<caption><h4>Claim status for " + row.get("name").toString().replace("\"", "") + ":</h4></caption>" +
+                                "<tr>" +
+                                    "<th>QuoteID</th>" +
+                                    "<th>Name</th>" +
+                                    "<th>Email</th>" +
+                                    "<th>Address</th>" +
+                                    "<th>US State</th>" +
+                                    "<th>License Plate</th>" +
+                                    "<th>Claim Status</th>" +
+                                "</tr>" +
+                                "<tr>" +
+                                    "<td>"+row.get("quoteid").toString().replace("\"", "")+"</th>" +
+                                    "<td>"+row.get("name").toString().replace("\"", "")+"</th>" +
+                                    "<td>"+row.get("email").toString().replace("\"", "")+"</th>" +
+                                    "<td>"+row.get("address").toString().replace("\"", "")+"</th>" +
+                                    "<td>"+row.get("usstate").toString().replace("\"", "")+"</th>" +
+                                    "<td>"+row.get("licenseplate").toString().replace("\"", "")+"</th>" +
+                                    "<td>"+row.get("claimstatus").toString().replace("\"", "")+"</th>" +
+                                "</tr>");
+                    } else {
+                        contentBuilder.append(str);
+                    }
+                }
             } catch (Throwable exception) {
                 exception.printStackTrace();
-            }
-            // TODO: EDIT/REMOVE
-            in = new BufferedReader(new FileReader("./src/main/resources/index.html"));
-            while ((str = in.readLine()) != null) {
-                if (str.equalsIgnoreCase("<h2>We're starting here!</h2>")) {
-                    contentBuilder.append("<h2>Searching the quote id: ").append(requestParamValue).append("</h2>");
-                } else {
-                    contentBuilder.append(str);
-                }
             }
         } else {
             System.out.println("Unsupported request");
