d12cf333cd06ecc2cd7d1328c51ab2cb168440a1
Daniel Pink <daniel.pink@uk.ibm.com>
2020-09-30 16:03:24 +0100

    Revert back to 2020.2.1 for ace integration server

--- products/bash/release-ace-integration-server.sh
+++ products/bash/release-ace-integration-server.sh
@@ -103,7 +103,8 @@ spec:
   designerFlowsOperationMode: disabled
   license:
     accept: true
-    license: L-APEH-BPUCJK
+    license: L-AMYG-BQ2E4U
+    # license: L-APEH-BPUCJK
     use: CloudPakForIntegrationProduction
   replicas: 2
   router:
@@ -111,7 +112,8 @@ spec:
   service:
     endpointType: http
   useCommonServices: true
-  version: 11.0.0.10-r1
+  version: 11.0.0.9
+  # version: 11.0.0.10-r1
   tracing:
     enabled: ${tracing_enabled}
     namespace: ${tracing_namespace}
