29f4915e0a3ffdbd42464f23fd46ed569b1a734f
James Kirk <jameskirk95@hotmail.com>
2020-10-15 13:52:30 +0100

    search for repeats of regions, error if > 1

--- products/bash/1-click-pre-validation.sh
+++ products/bash/1-click-pre-validation.sh
@@ -84,6 +84,12 @@ if [[ "$missingParams" == "true" ]]; then
   usage
 fi
 
+if [[ $(oc get node -o json | jq -r '.items[].metadata.labels["ibm-cloud.kubernetes.io/zone"]' | uniq | wc -l | xargs) != 1 ]]; then
+  divider
+  echo "ERROR: 1-click install does not currently support MZR clusters, please try again with nodes from the same region"
+  exit 1
+fi
+
 divider
 echo -e "$info Current directory: $CURRENT_DIR"
 echo -e "$info Common services admin username: $csDefaultAdminUser"
