9c80cb123857fc682ce15917e4d1ef55b6833972
Kinshuk Bhardwaj <kkb1993.kinshuk@gmail.com>
2020-12-08 15:54:41 +0000

    Replace demo json after 1-click done

--- products/bash/setup-demos.sh
+++ products/bash/setup-demos.sh
@@ -342,8 +342,6 @@ echo -e "$INFO Samples repo branch: '$SAMPLES_REPO_BRANCH'"
 echo -e "$INFO Demo version: '$DEMO_VERSION'"
 echo -e "$INFO Namespace: '$NAMESPACE'" && divider
 
-exit 0
-
 #-------------------------------------------------------------------------------------------------------------------
 # If all demos enabled then add all demos else delete all demos value and keep enabled ones
 #-------------------------------------------------------------------------------------------------------------------
