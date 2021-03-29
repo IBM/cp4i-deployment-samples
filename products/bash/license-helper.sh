f215dfe19d17cd568a7091155f1036ca59c7ed54
Thomas Li <tom9383@gmail.com>
2021-03-29 10:37:18 +0100

    Add default licenses to license helper
    Co-authored-by: laurensks <laurensks@users.noreply.github.com>

--- products/bash/license-helper.sh
+++ products/bash/license-helper.sh
@@ -1,25 +1,31 @@
 #!/bin/bash
-#******************************************************************************
+#*******************************************************************************
 # Licensed Materials - Property of IBM
 # (c) Copyright IBM Corporation 2020. All Rights Reserved.
 #
 # Note to U.S. Government Users Restricted Rights:
 # Use, duplication or disclosure restricted by GSA ADP Schedule
 # Contract with IBM Corp.
-#******************************************************************************
+#*******************************************************************************
 
-#******************************************************************************
+#*******************************************************************************
 # PREREQUISITES:
-#   - Logged into cluster on the OC CLI (https://docs.openshift.com/container-platform/4.4/cli_reference/openshift_cli/getting-started-cli.html)
+#   - Logged into cluster on the OC CLI (https://docs.openshift.com/container-platform/4.6/cli_reference/openshift_cli/getting-started-cli.html)
 #
 # USAGE:
-#   source ./license-helper.sh; getAllLicenses \$NAMESPACE
-#******************************************************************************
+#   source ./license-helper.sh; getAceLicense $NAMESPACE
+#*******************************************************************************
 
-LICENSES_CM=demo-licenses
+LICENSES_CM="demo-licenses"
+DEFAULT_ACE="L-APEH-BPUCJK"
+DEFAULT_APIC="L-RJON-BRSHKF"
+DEFAULT_AR="L-NCAN-BXWG76"
+DEFAULT_DEMO="L-RJON-BYRMYW"
+DEFAULT_MQ="L-RJON-BN7PN3"
+DEFAULT_TRACING="CP4I"
 
 function usage() {
-  echo "Usage: source $0; getAllLicenses \$NAMESPACE"
+  echo "Usage: source $0; getAceLicense \$NAMESPACE"
 }
 
 while getopts "" opt; do
@@ -31,30 +37,32 @@ while getopts "" opt; do
   esac
 done
 
-function getAllLicenses() {
-  oc -n $1 get configmap $LICENSES_CM -ojsonpath='{.data}'
-}
-
-function getDemoLicense() {
-  oc -n $1 get configmap $LICENSES_CM -ojsonpath='{.data.demo}'
-}
-
 function getACELicense() {
-  oc -n $1 get configmap $LICENSES_CM -ojsonpath='{.data.ace}'
+  ACE_LICENSE=$(oc -n $1 get configmap $LICENSES_CM -ojsonpath='{.data.ace}' 2> /dev/null)
+  [[ $? == 0 && !$ACE_LICENSE == '' ]] && echo $ACE_LICENSE || echo $DEFAULT_ACE
 }
 
 function getAPICLicense() {
-  oc -n $1 get configmap $LICENSES_CM -ojsonpath='{.data.apic}'
+  APIC_LICENSE=$(oc -n $1 get configmap $LICENSES_CM -ojsonpath='{.data.apic}' 2> /dev/null)
+  [[ $? == 0 && !$APIC_LICENSE == '' ]] && echo $APIC_LICENSE || echo $DEFAULT_APIC
 }
 
 function getARLicense() {
-  oc -n $1 get configmap $LICENSES_CM -ojsonpath='{.data.ar}'
+  AR_LICENSE=$(oc -n $1 get configmap $LICENSES_CM -ojsonpath='{.data.ar}' 2> /dev/null)
+  [[ $? == 0 && !$AR_LICENSE == '' ]] && echo $AR_LICENSE || echo $DEFAULT_AR
+}
+
+function getDemoLicense() {
+  DEMO_LICENSE=$(oc -n $1 get configmap $LICENSES_CM -ojsonpath='{.data.demo}' 2> /dev/null)
+  [[ $? == 0 && !$DEMO_LICENSE == '' ]] && echo $DEMO_LICENSE || echo $DEFAULT_DEMO
 }
 
 function getMQLicense() {
-  oc -n $1 get configmap $LICENSES_CM -ojsonpath='{.data.mq}'
+  MQ_LICENSE=$(oc -n $1 get configmap $LICENSES_CM -ojsonpath='{.data.mq}' 2> /dev/null)
+  [[ $? == 0 && !$MQ_LICENSE == '' ]] && echo $MQ_LICENSE || echo $DEFAULT_MQ
 }
 
 function getTracingLicense() {
-  oc -n $1 get configmap $LICENSES_CM -ojsonpath='{.data.tracing}'
+  TRACING_LICENSE=$(oc -n $1 get configmap $LICENSES_CM -ojsonpath='{.data.tracing}' 2> /dev/null)
+  [[ $? == 0 && !$TRACING_LICENSE == '' ]] && echo $TRACING_LICENSE || echo $DEFAULT_TRACING
 }
