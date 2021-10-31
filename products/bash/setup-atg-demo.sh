b0d8f104d1dfea1b414fb6e318e5645abe9cdfd0
Daniel Pink <64848511+DanRoseus@users.noreply.github.com>
2021-10-31 23:03:09 +0000

    Update APIC to 1854 build

--- products/bash/setup-atg-demo.sh
+++ products/bash/setup-atg-demo.sh
@@ -8,9 +8,9 @@
 # Contract with IBM Corp.
 #******************************************************************************
 
-# https://apiconnect-jenkins.swg-devops.com/job/velox-integration/job/apiconnect-operator/job/v10.0/1835/
-DP_CATALOG_SOURCE=ibmcom/datapower-operator-catalog@sha256:6063756ea93ea46ca93824578f6860d40fe1433df2bb869b5740904728da8021
-APIC_CATALOG_SOURCE=ibmcom/ibm-apiconnect-catalog@sha256:f2137f12e1b497594e499fe4fa00479ec717e7bea56a7bbcc3b8bc673ac08ea1
+# https://apiconnect-jenkins.swg-devops.com/job/velox-integration/job/apiconnect-operator/job/v10.0/1854/
+DP_CATALOG_SOURCE=ibmcom/datapower-operator-catalog@sha256:415837045b389c2be97711dee3d1d862d2b24685f57058ba474847f36661d4e7
+APIC_CATALOG_SOURCE=ibmcom/ibm-apiconnect-catalog@sha256:7a199f0f9f6440347e3614ff6c746b22f06e5f3f3fa3e57add31523960a48de3
 
 function divider() {
   echo -e "\n-------------------------------------------------------------------------------------------------------------------\n"
