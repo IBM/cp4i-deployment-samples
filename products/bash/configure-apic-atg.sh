4964c32c2dbf6703da24b883db0b9ad280b7a162
Daniel Pink <daniel.pink@uk.ibm.com>
2021-11-02 10:31:46 +0000

    Fix progress count

--- products/bash/configure-apic-atg.sh
+++ products/bash/configure-apic-atg.sh
@@ -63,7 +63,7 @@ for i in $(seq 1 720); do
     echo "[OK] APIC is ready"
     break
   else
-    echo "Waiting for APIC install to complete (Attempt $i of 120). Status: $APIC_STATUS"
+    echo "Waiting for APIC install to complete (Attempt $i of 720). Status: $APIC_STATUS"
     kubectl get apic -n $namespace
     kubectl get pods,pvc -n $namespace -l app.kubernetes.io/managed-by=ibm-apiconnect -l app.kubernetes.io/part-of=${release_name}
     echo "Checking again in one minute..."
@@ -92,7 +92,7 @@ for i in $(seq 1 360); do
     fi
   fi
 
-  echo "Waiting, checking again in one minute... (Attempt $i of 60)"
+  echo "Waiting, checking again in one minute... (Attempt $i of 360)"
   sleep 10
 done
 
