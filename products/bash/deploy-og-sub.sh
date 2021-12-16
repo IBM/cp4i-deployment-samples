4925983c802698d1f08551670491c43371122be2
Daniel Pink <daniel.pink@uk.ibm.com>
2021-12-16 11:23:31 +0000

    Revert to released channels and temporary disabling of APIC/ES operators

--- products/bash/deploy-og-sub.sh
+++ products/bash/deploy-og-sub.sh
@@ -80,34 +80,34 @@ done
 #   echo "${PRODUCT}_NAME=$NAME"
 #   echo "${PRODUCT}_CHANNEL=$CHANNEL"
 # done
-NAVIGATOR_CATALOG=pn-operators
+NAVIGATOR_CATALOG=ibm-operator-catalog
 NAVIGATOR_NAME=ibm-integration-platform-navigator
 NAVIGATOR_CHANNEL=v5.2
-ACE_CATALOG=ace-operators
+ACE_CATALOG=ibm-operator-catalog
 ACE_NAME=ibm-appconnect
 ACE_CHANNEL=v3.0
-APIC_CATALOG=apic-operators
+APIC_CATALOG=ibm-operator-catalog
 APIC_NAME=ibm-apiconnect
 APIC_CHANNEL=v2.4
-AR_CATALOG=ar-operators
+AR_CATALOG=ibm-operator-catalog
 AR_NAME=ibm-integration-asset-repository
 AR_CHANNEL=v1.4
-ASPERA_CATALOG=aspera-operators
+ASPERA_CATALOG=ibm-operator-catalog
 ASPERA_NAME=aspera-hsts-operator
 ASPERA_CHANNEL=v1.4
 DEMOS_CATALOG=cp4i-demo-operator-catalog-source
 DEMOS_NAME=ibm-integration-demos-operator
 DEMOS_CHANNEL=v1.0
-DP_CATALOG=dp-operators
+DP_CATALOG=ibm-operator-catalog
 DP_NAME=datapower-operator
 DP_CHANNEL=v1.6
-ES_CATALOG=es-operators
+ES_CATALOG=ibm-operator-catalog
 ES_NAME=ibm-eventstreams
 ES_CHANNEL=v2.5
-MQ_CATALOG=mq-operators
+MQ_CATALOG=ibm-operator-catalog
 MQ_NAME=ibm-mq
 MQ_CHANNEL=v1.7
-OD_CATALOG=od-operators
+OD_CATALOG=ibm-operator-catalog
 OD_NAME=ibm-integration-operations-dashboard
 OD_CHANNEL=v2.5
 
@@ -306,7 +306,8 @@ create_subscription ${namespace} ${NAVIGATOR_CATALOG} "${NAVIGATOR_NAME}" "$NAVI
 echo "INFO: Applying subscriptions for aspera, ace, event streams, mq, and asset repo"
 create_subscription ${namespace} ${ASPERA_CATALOG} "${ASPERA_NAME}" "${ASPERA_CHANNEL}"
 create_subscription ${namespace} ${ACE_CATALOG} "${ACE_NAME}" "${ACE_CHANNEL}"
-create_subscription ${namespace} ${ES_CATALOG} "${ES_NAME}" "${ES_CHANNEL}"
+# TODO Re-enable this once released
+#create_subscription ${namespace} ${ES_CATALOG} "${ES_NAME}" "${ES_CHANNEL}"
 create_subscription ${namespace} ${MQ_CATALOG} "${MQ_NAME}" "${MQ_CHANNEL}"
 create_subscription ${namespace} ${AR_CATALOG} "${AR_NAME}" "${AR_CHANNEL}"
 
@@ -321,7 +322,8 @@ if [[ "${EXTRA_SAFE_BUT_SLOW}" == "false" ]]; then
 fi
 
 echo "INFO: Apply the APIC/Tracing subscriptions"
-create_subscription ${namespace} ${APIC_CATALOG} "${APIC_NAME}" "${APIC_CHANNEL}"
+# TODO Re-enable this once released
+#create_subscription ${namespace} ${APIC_CATALOG} "${APIC_NAME}" "${APIC_CHANNEL}"
 create_subscription ${namespace} ${OD_CATALOG} "${OD_NAME}" "${OD_CHANNEL}"
 
 echo "INFO: Wait for all subscriptions to succeed"
