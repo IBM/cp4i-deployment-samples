e0f5ff9755d630b23df06fe7976e9f8309f6b4fd
Hasan Rizvi <hrizvi@uk.ibm.com>
2022-05-05 14:11:46 +0100

    calls made to zen-fix.sh file

--- products/bash/release-ar.sh
+++ products/bash/release-ar.sh
@@ -96,6 +96,7 @@ EOF
     exit 1
   fi
   echo "INFO: Waiting up to 10 minutes for AssetRepository to be created. Waited ${time} minute(s)."
+  $CURRENT_DIR/zen-fix.sh -n "$namespace"
   time=$((time + 1))
   sleep 60
 done
--- products/bash/zen-fix.sh
+++ products/bash/zen-fix.sh
@@ -21,16 +21,13 @@ function divider() {
 }
 
 function usage() {
-  echo "Usage: $0 -n <NAMESPACE> -c <CALLING_INSTANCE>"
+  echo "Usage: $0 -n <NAMESPACE>"
   divider
   exit 1
 }
 
-while getopts "n:c:" opt; do
+while getopts "n:" opt; do
   case ${opt} in
-  c)
-    CALLING_INSTANCE="$OPTARG"
-    ;;
   n)
     NAMESPACE="$OPTARG"
     ;;
@@ -48,37 +45,47 @@ if ! oc get namespace ${NAMESPACE} >/dev/null 2>&1; then
     exit 1
 fi
 
-PODS_TO_DELETE=$( oc get pod -n ${NAMESPACE} -o name | grep -iw 'zen-watcher\|ibm-nginx')
-
-if [ -z "${PODS_TO_DELETE}" ]; then
-    echo -e "[ERROR] zen-watcher and ibm-nginx pods not found"
+ZEN_WATCHER_POD=$(oc get pod -n ${NAMESPACE} -o name | grep -iw zen-watcher)
+if [ -z "${ZEN_WATCHER_POD}" ]; then
+    echo -e "[ERROR] zen-watcher pod not found"
     exit 1
 fi
 
-echo -e "[INFO] Deleting the ${PODS_TO_DELETE}"
+PANIC_FOUND=$(oc logs $ZEN_WATCHER_POD -n ${NAMESPACE} | grep -i panic)
+if [ ! -z "${PANIC_FOUND}" ]; then
+    echo -e "Panic found going to restart the zen-watcher pod"
 
-if ! oc delete ${PODS_TO_DELETE} -n ${NAMESPACE}; then
-    echo -e "[ERROR] Unable to delete the pods ${PODS_TO_DELETE}"
-    exit 1
-fi 
+    echo -e "[INFO] Deleting the ${ZEN_WATCHER_POD}"
 
-PODS_TO_RESTART=$( oc get pod -n ${NAMESPACE} -o name | grep -iw 'zen-watcher\|ibm-nginx')
+    if ! oc delete ${ZEN_WATCHER_POD} -n ${NAMESPACE}; then
+        echo -e "[ERROR] Unable to delete the pod ${ZEN_WATCHER_POD}"
+        exit 1
+    fi 
 
+    sleep 30 #wait few seconds for the pods to appear
 
-echo -e "[INFO] Waiting for zen-watcher and ibm-nginx pods ${PODS_TO_RESTART} to get ready"
+    PODS_TO_RESTART=$( oc get pod -n ${NAMESPACE} -o name | grep -iw 'zen-watcher')
 
-time=0
-
-for POD in ${PODS_TO_RESTART}; do
-    echo -e "INFO: Waiting for ${POD} to get ready"
-    while [[ "$(oc get ${POD} -n ${NAMESPACE} -o json | jq -r '.status.conditions[] | select(.type=="Ready").status')" != "True" ]]; do
-    echo "INFO: $(oc get ${POD} -n ${NAMESPACE})"
-    if [ $time -gt 90 ]; then
-        echo "ERROR: Exiting ${POD} in ${NAMESPACE} is not ready"
+    if [ -z "$PODS_TO_RESTART}" ]; then
+        echo -e "[ERROR] zen-watcher pod not found"
         exit 1
     fi
-    echo "INFO: Waiting up to 90 minutes for  ${POD} in ${NAMESPACE} to be ready. Waited ${time} minute(s)."
-    time=$((time + 1))
-    sleep 60
-    done
-done
+
+    echo -e "[INFO] Waiting for zen-watcher ${PODS_TO_RESTART} to get ready"
+
+    time=0
+    # wait for pods to get ready after the restart
+        while [[ "$(oc get ${PODS_TO_RESTART} -n ${NAMESPACE} -o json | jq -r '.status.conditions[] | select(.type=="Ready").status')" != "True" ]]; do
+        echo "INFO: $(oc get ${PODS_TO_RESTART} -n ${NAMESPACE})"
+        if [ $time -gt 5 ]; then
+            echo "ERROR: Exiting ${PODS_TO_RESTART} in ${NAMESPACE} is not ready"
+            exit 1
+        fi
+        echo "INFO: Waiting up to 5 minutes for  ${PODS_TO_RESTART} in ${NAMESPACE} to be ready. Waited ${time} minute(s)."
+        time=$((time + 1))
+        sleep 60
+        done
+  
+    oc get pod -n ${NAMESPACE} -o name | grep -iw 'zen-watcher'
+
+fi
