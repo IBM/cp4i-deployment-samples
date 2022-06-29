b32364022aba48f907385a8c81cbc1ed026ddffe
Daniel Pink <daniel.pink@uk.ibm.com>
2022-06-29 15:49:39 +0100

    Update readme and remove old scripts
    Signed-off-by: Daniel Pink <daniel.pink@uk.ibm.com>

--- products/bash/find-catalog-sources/get-latest.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-CASE_REPO_PATH=https://github.com/IBM/cloud-pak/raw/master/repo/case
-CASE_NAME=ibm-cp-integration
-CASE_VERSION=$(curl -s https://raw.githubusercontent.com/IBM/cloud-pak/master/repo/case/ibm-cp-integration/index.yaml | grep "appVersion: $VERSION" -B 1 | grep -v appVersion | tr -d " :" | sort --version-sort | tail -n 1)
-echo ${CASE_VERSION}
