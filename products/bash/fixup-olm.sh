b72295ac09b2773db2d47f30affde093cbe34133
Daniel Pink <daniel.pink@uk.ibm.com>
2022-12-16 14:40:21 +0000

    Improve fixup-olm.sh Add cmd flags to set namespace and enable dry run mode Add extra condition for detecting if subscription is failing, make sure status.state not set Recreate any deleted subscriptions
    Signed-off-by: Daniel Pink <danielpink@Daniels-MacBook-Pro.local>
    Signed-off-by: Lauren Stockley <lauren.stockley@ibm.com>

--- products/bash/fixup-olm.sh
+++ products/bash/fixup-olm.sh
@@ -3,9 +3,21 @@
 OLM_NS="openshift-operator-lifecycle-manager"
 CATALOG_OPERATOR_LABEL="app=catalog-operator"
 OLM_OPERATOR_LABEL="app=olm-operator"
-ONE_CLICK_NAMESPACE="cp4i"
+CP4I_NAMESPACE="cp4i"
 CS_NAMESPACE="ibm-common-services"
-DRY_RUN=true
+DRY_RUN=false
+
+while getopts "dn:" opt; do
+  case ${opt} in
+  d)
+    DRY_RUN=true
+    ;;
+  n)
+    CP4I_NAMESPACE="$OPTARG"
+    ;;
+  esac
+done
+
 
 echo "Checking OLM..."
 CATALOG_RESTART_COUNT=$(oc get pod -n $OLM_NS -l $CATALOG_OPERATOR_LABEL -o jsonpath='{.items[0].status.containerStatuses[0].restartCount}')
@@ -26,8 +38,8 @@ fi
 
 
 echo "Checking operator subscriptions..."
-# Get a list of all subscriptions that have the ResolutionFailed condition
-rows=$(oc get subscription --all-namespaces -o json | jq -r '.items[] | select(.status.conditions[].type == "ResolutionFailed") | {namespace:.metadata.namespace, name:.metadata.name, currentCSV:.status.currentCSV} | @base64')
+# Get a list of all subscriptions that have the ResolutionFailed condition and don't have a status.state
+rows=$(oc get subscription --all-namespaces -o json | jq -r '.items[] | select(.status.conditions[].type == "ResolutionFailed") | select(.status.state == null) | {namespace:.metadata.namespace, name:.metadata.name, currentCSV:.status.currentCSV} | @base64')
 for row in ${rows}; do
   _jq() {
    echo ${row} | base64 --decode | jq -r ${1}
@@ -36,7 +48,7 @@ for row in ${rows}; do
   # Check if the namespace is for 1-click or CS
   subscription_namespace="$(_jq '.namespace')"
   subscription_name="$(_jq '.name')"
-  if [[ "$subscription_namespace" == "$ONE_CLICK_NAMESPACE" ]] || [[ "$subscription_namespace" == "$CS_NAMESPACE" ]]; then
+  if [[ "$subscription_namespace" == "$CP4I_NAMESPACE" ]] || [[ "$subscription_namespace" == "$CS_NAMESPACE" ]]; then
     # Auto delete the csv/subscription
     current_csv=$(_jq '.currentCSV')
     if [[ ! -z "${current_csv}" ]] && [[ "${current_csv}" != "null" ]] ; then
@@ -46,13 +58,19 @@ for row in ${rows}; do
         echo "The csv named [${current_csv}] in the [$(_jq '.namespace')] namespace needs to be deleted."
       fi
     fi
+    SUBSCRIPTION_JSON="$(oc get subscription -n ${subscription_namespace} ${subscription_name} -o json | jq 'del(.status) | del(.metadata.managedFields) | del(.metadata.creationTimestamp) | del(.metadata.uid) | del(.metadata.resourceVersion)')"
     if [[ "${DRY_RUN}" == "false" ]]; then
       oc delete subscription -n ${subscription_namespace} ${subscription_name}
-      if [[ "$subscription_namespace" == "$CS_NAMESPACE" ]]; then
-        sleep 5
-      fi
+
+      sleep 5
+
+      cat <<EOF | oc apply -f -
+${SUBSCRIPTION_JSON}
+EOF
     else
       echo "The subscription named [${subscription_name}] in the [${subscription_namespace}] namespace needs to be deleted and recreated."
+      echo "Use this json:"
+      echo "$SUBSCRIPTION_JSON"
     fi
   else
     echo "The operator named [${subscription_name}] in the [${subscription_namespace}] namespace needs to be deleted and re-installed."
