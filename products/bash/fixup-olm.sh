ffc618e871239341a15808782aea2da51de4649f
Daniel Pink <danielpink@Daniels-MacBook-Pro.local>
2022-12-20 08:53:46 +0000

    Fix detection of csv associated with subscription
    Signed-off-by: Daniel Pink <danielpink@Daniels-MacBook-Pro.local>

--- products/bash/fixup-olm.sh
+++ products/bash/fixup-olm.sh
@@ -7,6 +7,10 @@ CP4I_NAMESPACE="cp4i"
 CS_NAMESPACE="ibm-common-services"
 DRY_RUN=false
 
+# Subscriptions/CSVs will only be deleted if older than the following threshold.
+# This gives OLM a chance to install and resolve any problems itself.
+AGE_THRESHOLD_SECS=60
+
 while getopts "dn:" opt; do
   case ${opt} in
   d)
@@ -49,41 +53,42 @@ for row in ${rows}; do
   subscription_namespace="$(_jq '.namespace')"
   subscription_name="$(_jq '.name')"
   if [[ "$subscription_namespace" == "$CP4I_NAMESPACE" ]] || [[ "$subscription_namespace" == "$CS_NAMESPACE" ]]; then
-    # Auto delete the csv/subscription
-    current_csv=$(_jq '.currentCSV')
-    if [[ ! -z "${current_csv}" ]] && [[ "${current_csv}" != "null" ]] ; then
-      if [[ "${DRY_RUN}" == "false" ]]; then
-        oc delete csv -n ${subscription_namespace} ${current_csv}
-      else
-        echo "The csv named [${current_csv}] in the [$(_jq '.namespace')] namespace needs to be deleted."
-      fi
+    # Check the creation time against the current time and only delete if it's had > 1 minute
+    CREATION_TIMESTAMP=$(oc get subscription -n ${subscription_namespace} ${subscription_name} -o json | jq -r .metadata.creationTimestamp)
+    CURRENT_TIMESTAMP=$(date -u +"%Y-%m-%dT%H:%M:%SZ")
+
+    if [[ "$OSTYPE" == "darwin"* ]]; then
+      CREATION_TIMESTAMP_SECS=$(date -jf "%Y-%m-%dT%H:%M:%SZ" "${CREATION_TIMESTAMP}" +%s)
+      CURRENT_TIMESTAMP_SECS=$(date -jf "%Y-%m-%dT%H:%M:%SZ" "${CURRENT_TIMESTAMP}" +%s)
+    else
+      CREATION_TIMESTAMP_SECS=$(date -d"${CREATION_TIMESTAMP}" +%s)
+      CURRENT_TIMESTAMP_SECS=$(date -d"${CURRENT_TIMESTAMP}" +%s)
     fi
-    if [[ "${DRY_RUN}" == "false" ]]; then
-      # Check the creation time against the current time and only delete if it's had > 1 minute
-      CREATION_TIMESTAMP=$(oc get subscription -n ${subscription_namespace} ${subscription_name} -o json | jq -r .metadata.creationTimestamp)
-      CURRENT_TIMESTAMP=$(date -u +"%Y-%m-%dT%H:%M:%SZ")
 
-      if [[ "$OSTYPE" == "darwin"* ]]; then
-        CREATION_TIMESTAMP_SECS=$(date -jf "%Y-%m-%dT%H:%M:%SZ" "${CREATION_TIMESTAMP}" +%s)
-        CURRENT_TIMESTAMP_SECS=$(date -jf "%Y-%m-%dT%H:%M:%SZ" "${CURRENT_TIMESTAMP}" +%s)
-      else
-        CREATION_TIMESTAMP_SECS=$(date -d"${CREATION_TIMESTAMP}" +%s)
-        CURRENT_TIMESTAMP_SECS=$(date -d"${CURRENT_TIMESTAMP}" +%s)
+    AGE="$(($CURRENT_TIMESTAMP_SECS-$CREATION_TIMESTAMP_SECS))"
+    if [ "$AGE" -ge "$AGE_THRESHOLD_SECS" ]; then
+      # Auto delete the csv/subscription
+      current_csv=$(_jq '.status.currentCSV')
+      if [[ ! -z "${current_csv}" ]] && [[ "${current_csv}" != "null" ]] ; then
+        if [[ "${DRY_RUN}" == "false" ]]; then
+          oc delete csv -n ${subscription_namespace} ${current_csv}
+        else
+          echo "The csv named [${current_csv}] in the [${subscription_namespace}] namespace needs to be deleted."
+        fi
       fi
 
-      AGE="$(($CURRENT_TIMESTAMP_SECS-$CREATION_TIMESTAMP_SECS))"
-      if [ "$AGE" -ge "60" ]; then
+      if [[ "${DRY_RUN}" == "false" ]]; then
         SUBSCRIPTION_JSON="$(oc get subscription -n ${subscription_namespace} ${subscription_name} -o json | jq 'del(.status) | del(.metadata.managedFields) | del(.metadata.creationTimestamp) | del(.metadata.uid) | del(.metadata.resourceVersion)')"
         oc delete subscription -n ${subscription_namespace} ${subscription_name}
 
         sleep 5
 
         cat <<EOF | oc apply -f -
-${SUBSCRIPTION_JSON}
+  ${SUBSCRIPTION_JSON}
 EOF
+      else
+        echo "The subscription named [${subscription_name}] in the [${subscription_namespace}] namespace needs to be deleted and recreated."
       fi
-    else
-      echo "The subscription named [${subscription_name}] in the [${subscription_namespace}] namespace needs to be deleted and recreated."
     fi
   else
     echo "The operator named [${subscription_name}] in the [${subscription_namespace}] namespace needs to be deleted and re-installed."
