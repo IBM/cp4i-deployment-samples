af065a5819a4e88845d75b8594a5e911d14c22e5
Daniel Pink <danielpink@Daniels-MacBook-Pro.local>
2023-01-16 11:45:40 +0000

    Use IntegrationRuntime for all DDD pipelines
    Signed-off-by: Daniel Pink <danielpink@Daniels-MacBook-Pro.local>
    Signed-off-by: Lauren Stockley <lauren.stockley@ibm.com>

--- products/bash/release-ace-integration-runtime.sh
+++ products/bash/release-ace-integration-runtime.sh
@@ -106,124 +106,16 @@ EOF
 )
 OCApplyYAML "$namespace" "$YAML"
 
-echo "TODO How to wait for the runtime to be ready?"
-
-exit 0
-
-timer=0
-echo "[INFO] tracing is set to $tracing_enabled"
-if [ "$tracing_enabled" == "true" ]; then
-  while ! oc get secrets icp4i-od-store-cred -n ${namespace}; do
-    echo "Waiting for the secret icp4i-od-store-cred to get created"
-    if [ $timer -gt 30 ]; then
-      echo "Secret icp4i-od-store-cred didn't get created in  ${namespace}, going to create the secret next "
-      break
-      timer=$((timer + 1))
-    fi
-    sleep 10
-  done
-fi
-# -------------------------------------- INSTALL JQ ---------------------------------------------------------------------
-
-divider
-
-echo -e "\nINFO: Checking if jq is pre-installed..."
-jqInstalled=false
-jqVersionCheck=$(jq --version)
-
-if [ $? -ne 0 ]; then
-  jqInstalled=false
-  JQ=./jq
-else
-  jqInstalled=true
-  JQ=jq
-fi
-
-if [[ "$jqInstalled" == "false" ]]; then
-  echo "INFO: JQ is not installed, installing jq..."
-  if [[ "$OSTYPE" == "linux-gnu"* ]]; then
-    echo "INFO: Installing on linux"
-    wget -O jq https://github.com/stedolan/jq/releases/download/jq-1.6/jq-linux64
-    chmod +x ./jq
-  elif [[ "$OSTYPE" == "darwin"* ]]; then
-    echo "INFO: Installing on MAC"
-    /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
-    brew install jq
-    JQ=jq
-  fi
-fi
-
-echo -e "\nINFO: Installed JQ version is $($JQ --version)"
-
-divider
-
-# -------------------------------------- FIND TOTAL ACE REPLICAS DEPLOYED -----------------------------------------------
-
-numberOfReplicas=$(oc get integrationservers $is_release_name -n $namespace -o json | $JQ -r '.spec.replicas')
-echo "INFO: Number of Replicas for '$is_release_name' is $numberOfReplicas"
-echo -e "\nINFO: Total number of ACE integration server '$is_release_name' related pods after deployment should be $numberOfReplicas"
-divider
-
-# -------------------------------------- CHECK FOR NEW IMAGE DEPLOYMENT STATUS ------------------------------------------
-
-echo "INFO: Image tag for '$is_release_name' is '$imageTag'"
-
-numberOfMatchesForImageTag=0
-time=0
-
-# wait for 10 minutes for all replica pods to be deployed with new image
-while [ "$numberOfMatchesForImageTag" -ne "$numberOfReplicas" ]; do
-  if [ $time -gt 90 ]; then
-    echo "ERROR: Timed-out trying to wait for all $is_release_name demo pods to be deployed with a new image containing the image tag '$imageTag'"
-    divider
-    exit 1
-  fi
-
-  numberOfMatchesForImageTag=0
-
-  if [ "${tracing_enabled}" == "true" ]; then
-    allCorrespondingPods=$(oc get pods -n $namespace | grep $is_release_name | grep 3/3 | grep Running | awk '{print $1}')
-  else
-    allCorrespondingPods=$(oc get pods -n $namespace | grep $is_release_name | grep 1/1 | grep Running | awk '{print $1}')
-  fi
-
-  echo -e "[INFO] Total pods for ACE Integration Server:\n$allCorrespondingPods"
-
-  echo -e "\nINFO: For ACE Integration server '$is_release_name':"
-  for eachAcePod in $allCorrespondingPods; do
-    imageInPod=$(oc get pod $eachAcePod -n $namespace -o json | $JQ -r '.spec.containers[0].image')
-    echo "INFO: Image present in the pod '$eachAcePod' is '$imageInPod'"
-    if [[ $imageInPod == *:$imageTag ]]; then
-      echo "INFO: Image tag matches.."
-      numberOfMatchesForImageTag=$((numberOfMatchesForImageTag + 1))
-    else
-      echo "INFO: Image tag '$imageTag' is not present in the image of the pod '$eachAcePod'"
-    fi
-  done
-
-  echo -e "\nINFO: Total $is_release_name demo pods deployed with new image: $numberOfMatchesForImageTag"
-  echo -e "\nINFO: All current $is_release_name demo pods are:\n"
-  oc get pods -n $namespace | grep $is_release_name | grep Running
-  if [[ $? -eq 1 ]]; then
-    echo -e "No pods found for $is_release_name yet"
-  fi
-  if [[ $numberOfMatchesForImageTag != "$numberOfReplicas" ]]; then
-    echo -e "\nINFO: Not all $is_release_name pods have been deployed with the new image having the image tag '$imageTag', retrying for upto 10 minutes for new $is_release_name demo pods to be deployed with new image. Waited ${time} minute(s)."
-    sleep 10
-  else
-    echo -e "\nINFO: All $is_release_name demo pods have been deployed with the new image"
-  fi
-  time=$((time + 1))
-  divider
-done
+echo "Wait for the runtime to be ready"
+oc wait --for=condition=ready IntegrationRuntime -n {namespace} ${release_name} --timeout 10m
 
 GOT_SERVICE=false
 for i in $(seq 1 30); do
-  if oc get svc ${is_release_name}-is -n ${namespace}; then
+  if oc get svc ${release_name}-ir -n ${namespace}; then
     GOT_SERVICE=true
     break
   else
-    echo "Waiting for ace api service named '${is_release_name}-is' (Attempt $i of 30)."
+    echo "Waiting for ace api service named '${release_name}-ir' (Attempt $i of 30)."
     echo "Checking again in 10 seconds..."
     sleep 10
   fi
