33b12317c8cbaeca559814d278c86b5fb38f701c
Lauren Stockley <lauren.stockley@ibm.com>
2023-01-18 15:48:20 +0000

    update the catalogs again
    Signed-off-by: Lauren Stockley <lauren.stockley@ibm.com>

--- products/bash/create-catalog-sources.sh
+++ products/bash/create-catalog-sources.sh
@@ -43,8 +43,8 @@ metadata:
   name: ibm-appconnect-catalog
   namespace: openshift-marketplace
 spec:
-  displayName: "ACE Operators 6.2.0"
-  image: icr.io/cpopen/appconnect-operator-catalog@sha256:932919a5aeb07d00035f4b30e31df4825fba202e061df58f0c5a9af4b084b841
+  displayName: "ACE Operators 7.0.0"
+  image: icr.io/cpopen/appconnect-operator-catalog@sha256:89d67d6a6934f056705000855bac890f6699435a475b690c143387a5c6a1352c
   publisher: IBM
   sourceType: grpc
   updateStrategy:
@@ -99,8 +99,8 @@ metadata:
   name: ibm-datapower-operator-catalog
   namespace: openshift-marketplace
 spec:
-  displayName: "DP Operators 1.6.4"
-  image: icr.io/cpopen/datapower-operator-catalog@sha256:aa1872bcebcc9912823821ff34376fddd016e2bcdd76e0298b51c3187669169f
+  displayName: "DP Operators 1.6.5"
+  image: icr.io/cpopen/datapower-operator-catalog@sha256:244827e90e194fc92e0d60d5da7ec434d2711139ea1392f816a05bde83da386a
   publisher: IBM
   sourceType: grpc
   updateStrategy:
@@ -113,8 +113,8 @@ metadata:
   name: ibm-eventstreams-catalog
   namespace: openshift-marketplace
 spec:
-  displayName: "ES Operators v3.1.1"
-  image: icr.io/cpopen/ibm-eventstreams-catalog@sha256:fb4a1b232ab796b2ae75616e071415ad17582d0539727f532abf6416f07403f5
+  displayName: "ES Operators v3.1.3"
+  image: icr.io/cpopen/ibm-eventstreams-catalog@sha256:803f7f8de9d3e2d52878ec78da6991917cfe21af937ab39009e2f218bf6ac0a1
   publisher: IBM
   sourceType: grpc
   updateStrategy:
@@ -141,8 +141,8 @@ metadata:
   name: ibm-integration-operations-dashboard-catalog
   namespace: openshift-marketplace
 spec:
-  displayName: "OD Operators 2.6.5"
-  image: icr.io/cpopen/ibm-integration-operations-dashboard-catalog@sha256:12e7be1060f015286a5b3e16054d08e09068d02fb16a95f03f581a9ce82b3ebe
+  displayName: "OD Operators 2.6.6"
+  image: icr.io/cpopen/ibm-integration-operations-dashboard-catalog@sha256:adca1ed1a029ec648c665e9e839da3a7d20e533809706b72e7e676c665d2d7b3
   publisher: IBM
   sourceType: grpc
   updateStrategy:
@@ -169,8 +169,8 @@ metadata:
   name: ibm-mq-operator-catalog
   namespace: openshift-marketplace
 spec:
-  displayName: "MQ Operators v2.2.0"
-  image: icr.io/cpopen/ibm-mq-operator-catalog@sha256:b1d90cc6a3bf02b70668d0c07402a47258f85792ffca67d5ce73e5e34fde297d
+  displayName: "MQ Operators v2.2.1"
+  image: icr.io/cpopen/ibm-mq-operator-catalog@sha256:8de83ff5531de8df3ca639f612480f95ccd39a26e85a1bbdf18c7375dc93917a
   publisher: IBM
   sourceType: grpc
   updateStrategy:
