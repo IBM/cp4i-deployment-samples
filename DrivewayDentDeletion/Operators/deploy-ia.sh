ebcf111514b656c3d497a4092e87a2b4476f7e7c
Daniel Pink <daniel.pink@uk.ibm.com>
2023-05-17 13:38:02 +0100

    Some updates

--- DrivewayDentDeletion/Operators/deploy-ia.sh
+++ DrivewayDentDeletion/Operators/deploy-ia.sh
@@ -24,11 +24,11 @@ CURRENT_DIR=$(dirname $0)
 source $CURRENT_DIR/../../products/bash/utils.sh
 
 NAMESPACE="cp4i"
-DDD_DEMO_TYPE="dev"
 BLOCK_STORAGE_CLASS="cp4i-block-performance"
 FILE_STORAGE_CLASS="cp4i-file-performance-gid"
+DDD_ENV="dev"
 
-while getopts "b:f:n:e:u:t" opt; do
+while getopts "b:f:n:e:u" opt; do
   case ${opt} in
   b)
     BLOCK_STORAGE_CLASS="$OPTARG"
@@ -38,9 +38,6 @@ while getopts "b:f:n:e:u:t" opt; do
     ;;
   n)
     NAMESPACE="$OPTARG"
-    ;;
-  t)
-    DDD_DEMO_TYPE="test"
     ;;
   e)
     DDD_ENV="$OPTARG"
@@ -54,8 +51,8 @@ while getopts "b:f:n:e:u:t" opt; do
   esac
 done
 
-IM_NAME=ddd-${DDD_DEMO_TYPE}
-QM_NAME=mq-ddd-qm-${DDD_DEMO_TYPE}
+IA_NAME=ddd-${DDD_ENV}
+QM_NAME=mq-ddd-qm-${DDD_ENV}
 CONFIGURATIONS="[keystore-ddd, policyproject-ddd-${DDD_ENV}, serverconf-ddd, setdbparms-ddd, application-ddd-${DDD_ENV}, barauth-empty]"
 API_FILE='["'${BASE_URL}/DrivewayDentDeletion/Bar_files/ace-api/DrivewayDemo.bar'"]'
 ACME_FILE='["'${BASE_URL}/DrivewayDentDeletion/Bar_files/ace-acme/AcmeV1.bar'"]'
@@ -63,25 +60,6 @@ BERNIE_FILE='["'${BASE_URL}/DrivewayDentDeletion/Bar_files/ace-bernie/BernieV1.b
 CHRIS_FILE='["'${BASE_URL}/DrivewayDentDeletion/Bar_files/ace-chris/CrumpledV1.bar'"]'
 
 YAML=$(cat <<EOF
-apiVersion: v1
-kind: ConfigMap
-metadata:
-  name: qm-${QM_NAME}-default
-  labels:
-    app.kubernetes.io/component: ibm-mq
-    app.kubernetes.io/instance: ${NAMESPACE}.${IM_NAME}
-    app.kubernetes.io/managed-by: ibm-integration-platform-navigator-operator
-    app.kubernetes.io/name: integration-assembly
-    app.kubernetes.io/part-of: ${NAMESPACE}.${IM_NAME}
-data:
-  myqm.ini: "Service:\n\tName=AuthorizationService\n\tEntryPoints=14\n\tSecurityPolicy=UserExternal"
-  myqm.mqsc: |-
-    DEFINE CHANNEL('MTLS.SVRCONN') CHLTYPE(SVRCONN) SSLCAUTH(REQUIRED) SSLCIPH('ANY_TLS12_OR_HIGHER') REPLACE
-    ALTER QMGR CONNAUTH(' ')
-    REFRESH SECURITY
-    SET CHLAUTH('MTLS.SVRCONN') TYPE(SSLPEERMAP) SSLPEER('CN=*') USERSRC(NOACCESS) ACTION(REPLACE)
-    SET CHLAUTH('*') TYPE(ADDRESSMAP) ADDRESS('*') USERSRC(NOACCESS) ACTION(REPLACE)
----
 apiVersion: v1
 kind: ConfigMap
 metadata:
@@ -100,79 +78,15 @@ data:
     SET AUTHREC PROFILE('BumperOut') PRINCIPAL('app1') OBJTYPE(QUEUE) AUTHADD(BROWSE,GET,INQ,PUT)
     SET AUTHREC PROFILE('CrumpledIn') PRINCIPAL('app1') OBJTYPE(QUEUE) AUTHADD(BROWSE,GET,INQ,PUT)
     SET AUTHREC PROFILE('CrumpledOut') PRINCIPAL('app1') OBJTYPE(QUEUE) AUTHADD(BROWSE,GET,INQ,PUT)
-    SET CHLAUTH('MTLS.SVRCONN') TYPE(SSLPEERMAP) SSLPEER('CN=${NAMESPACE}.${IM_NAME},OU=my-team') USERSRC(MAP) MCAUSER('app1') ACTION(REPLACE)
+    SET CHLAUTH('MTLS.SVRCONN') TYPE(SSLPEERMAP) SSLPEER('CN=${NAMESPACE}.${IA_NAME},OU=my-team') USERSRC(MAP) MCAUSER('app1') ACTION(REPLACE)
     SET AUTHREC PRINCIPAL('app1') OBJTYPE(QMGR) AUTHADD(CONNECT,INQ)
     REFRESH SECURITY
     ALTER QMGR DEADQ(SYSTEM.DEAD.LETTER.QUEUE)
 ---
-apiVersion: cert-manager.io/v1
-kind: Issuer
-metadata:
-  name: ia-${NAMESPACE}-${IM_NAME}-ca
-  labels:
-    app.kubernetes.io/component: ibm-mq
-    app.kubernetes.io/instance: ${NAMESPACE}.${IM_NAME}
-    app.kubernetes.io/managed-by: ibm-integration-platform-navigator-operator
-    app.kubernetes.io/name: integration-assembly
-    app.kubernetes.io/part-of: ${NAMESPACE}.${IM_NAME}
-spec:
-  selfSigned: {}
----
-apiVersion: cert-manager.io/v1
-kind: Certificate
-metadata:
-  name: ia-${NAMESPACE}-${IM_NAME}-ca
-  labels:
-    app.kubernetes.io/component: ibm-mq
-    app.kubernetes.io/instance: ${NAMESPACE}.${IM_NAME}
-    app.kubernetes.io/managed-by: ibm-integration-platform-navigator-operator
-    app.kubernetes.io/name: integration-assembly
-    app.kubernetes.io/part-of: ${NAMESPACE}.${IM_NAME}
-spec:
-  commonName: ca
-  isCA: true
-  issuerRef:
-    group: cert-manager.io
-    kind: Issuer
-    name: ia-${NAMESPACE}-${IM_NAME}-ca
-  secretName: ia-${NAMESPACE}-${IM_NAME}-ca
----
-apiVersion: cert-manager.io/v1
-kind: Issuer
-metadata:
-  name: qm-${QM_NAME}-issuer
-  labels:
-    app.kubernetes.io/component: ibm-mq
-    app.kubernetes.io/instance: ${NAMESPACE}.${IM_NAME}
-    app.kubernetes.io/managed-by: ibm-integration-platform-navigator-operator
-    app.kubernetes.io/name: integration-assembly
-    app.kubernetes.io/part-of: ${NAMESPACE}.${IM_NAME}
-spec:
-  ca:
-    secretName: ia-${NAMESPACE}-${IM_NAME}-ca
----
-apiVersion: cert-manager.io/v1
-kind: Certificate
-metadata:
-  name: qm-${QM_NAME}-server
-  labels:
-    app.kubernetes.io/component: ibm-mq
-    app.kubernetes.io/instance: ${NAMESPACE}.${IM_NAME}
-    app.kubernetes.io/managed-by: ibm-integration-platform-navigator-operator
-    app.kubernetes.io/name: integration-assembly
-    app.kubernetes.io/part-of: ${NAMESPACE}.${IM_NAME}
-spec:
-  commonName: cert
-  issuerRef:
-    group: cert-manager.io
-    kind: Issuer
-    name: qm-${QM_NAME}-issuer
-  secretName: qm-${QM_NAME}-server
----
 apiVersion: integration.ibm.com/v1beta1
 kind: IntegrationAssembly
 metadata:
-  name: ${IM_NAME}
+  name: ${IA_NAME}
 spec:
   version: next
   license:
@@ -190,23 +104,12 @@ spec:
       metadata:
         name: ${QM_NAME}
       spec:
-        version: 9.3.1.0-r3
+        license:
+          license: L-YBXJ-ADJNSM
+          accept: true
+          use: NonProduction
         web:
           enabled: true
-        pki:
-          keys:
-          - name: default
-            secret:
-              items:
-              - tls.key
-              - tls.crt
-              secretName: qm-${QM_NAME}-server
-          trust:
-          - name: rootca
-            secret:
-              items:
-              - ca.crt
-              secretName: qm-${QM_NAME}-server
         queueManager:
           mqsc:
             - configMap:
@@ -221,28 +124,28 @@ spec:
     list:
     - kind: IntegrationRuntime
       metadata:
-        name: ${IM_NAME}-ace-api
+        name: ${IA_NAME}-ace-api
       spec:
         logFormat: basic
         barURL: ${API_FILE}
         configurations: ${CONFIGURATIONS}
     - kind: IntegrationRuntime
       metadata:
-        name: ${IM_NAME}-ace-acme
+        name: ${IA_NAME}-ace-acme
       spec:
         logFormat: basic
         barURL: ${ACME_FILE}
         configurations: ${CONFIGURATIONS}
     - kind: IntegrationRuntime
       metadata:
-        name: ${IM_NAME}-ace-bernie
+        name: ${IA_NAME}-ace-bernie
       spec:
         logFormat: basic
         barURL: ${BERNIE_FILE}
         configurations: ${CONFIGURATIONS}
     - kind: IntegrationRuntime
       metadata:
-        name: ${IM_NAME}-ace-chris
+        name: ${IA_NAME}-ace-chris
       spec:
         logFormat: basic
         barURL: ${CHRIS_FILE}
@@ -253,7 +156,7 @@ kind: Certificate
 metadata:
   name: qm-${QM_NAME}-client
 spec:
-  commonName: ${NAMESPACE}.${IM_NAME}
+  commonName: ${NAMESPACE}.${IA_NAME}
   subject:
     organizationalUnits:
     - my-team
