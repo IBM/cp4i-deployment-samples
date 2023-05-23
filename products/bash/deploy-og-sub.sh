a367be2d11f0bbe7a93b7b39c51fcbb488a1d5d8
Daniel Pink <daniel.pink@uk.ibm.com>
2023-05-23 09:13:17 +0100

    Remove debug stuff
    Signed-off-by: Lauren Stockley <lauren.stockley@ibm.com>

--- products/bash/deploy-og-sub.sh
+++ products/bash/deploy-og-sub.sh
@@ -102,8 +102,6 @@ function wait_for_all_subscriptions() {
         exit 1
       fi
 
-      # $CURRENT_DIR/fixup-olm.sh -n $namespace
-
       echo "Retrying in ${wait_time} seconds, waited for $(output_time $time) so far"
       ((time = time + $wait_time))
       sleep $wait_time
