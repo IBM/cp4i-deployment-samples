611b046b5f1c004d77234bb48d6a627bdc5647fe
Daniel Pink <daniel.pink@uk.ibm.com>
2023-06-16 10:42:02 +0100

    Update to released catalsources
    Signed-off-by: Lauren Stockley <lauren.stockley@ibm.com>

--- products/bash/find-catalog-sources/find-catalog-images.sh
+++ products/bash/find-catalog-sources/find-catalog-images.sh
@@ -2,18 +2,18 @@
 
 SCRIPT_DIR=$(dirname $0)
 
-# Data copied from the table in https://www.ibm.com/docs/en/cloud-paks/cp-integration/2022.4?topic=images-adding-catalog-sources-cluster
-CASES="IBM Cloud Pak for Integration	export CASE_NAME=ibm-integration-platform-navigator && export CASE_VERSION=7.0.4
-IBM Automation foundation assets	export CASE_NAME=ibm-integration-asset-repository && export CASE_VERSION=1.5.8
-IBM Cloud Pak for Integration Operations Dashboard	export CASE_NAME=ibm-integration-operations-dashboard && export CASE_VERSION=2.6.11
-IBM API Connect	export CASE_NAME=ibm-apiconnect && export CASE_VERSION=4.0.4
-IBM App Connect	export CASE_NAME=ibm-appconnect && export CASE_VERSION=8.1.0
-IBM MQ	export CASE_NAME=ibm-mq && export CASE_VERSION=2.3.3
-IBM Event Streams	export CASE_NAME=ibm-eventstreams && export CASE_VERSION=1.7.6
-IBM DataPower Gateway	export CASE_NAME=ibm-datapower-operator && export CASE_VERSION=1.6.7
-IBM Aspera HSTS	export CASE_NAME=ibm-aspera-hsts-operator && export CASE_VERSION=1.5.7
-IBM Cloud Pak foundational services	export CASE_NAME=ibm-cp-common-services && export CASE_VERSION=1.19.4"
-
+# Data copied from the table in https://www.ibm.com/docs/en/cloud-paks/cp-integration/2023.2?topic=images-adding-catalog-sources-cluster
+# + the row from https://ibm-cloud.slack.com/archives/CT320FAH4/p1686903900324049?thread_ts=1686887428.360699&cid=CT320FAH4
+CASES="IBM Cloud Pak for Integration	export CASE_NAME=ibm-integration-platform-navigator && export CASE_VERSION=7.1.0
+IBM Automation foundation assets	export CASE_NAME=ibm-integration-asset-repository && export CASE_VERSION=1.5.9
+IBM API Connect	export CASE_NAME=ibm-apiconnect && export CASE_VERSION=5.0.0
+IBM App Connect	export CASE_NAME=ibm-appconnect && export CASE_VERSION=8.2.1
+IBM MQ	export CASE_NAME=ibm-mq && export CASE_VERSION=2.4.0
+IBM Event Streams	export CASE_NAME=ibm-eventstreams && export CASE_VERSION=3.2.0
+IBM DataPower Gateway	export CASE_NAME=ibm-datapower-operator && export CASE_VERSION=1.7.0
+IBM Aspera HSTS	export CASE_NAME=ibm-aspera-hsts-operator && export CASE_VERSION=1.5.8
+IBM Cloud Pak foundational services	export CASE_NAME=ibm-cp-common-services && export CASE_VERSION=1.19.4
+IBM Event Endpoint Management	export CASE_NAME=ibm-eventendpointmanagement && export CASE_VERSION=11.0.0"
 IFS='
 '
 export ARCH=amd64
