0af1c262539cfe717972c0a823e9ea3cfd9173eb
Drew Douglass <drewd@us.ibm.com>
2024-03-21 11:11:29 -0400

    Update deploy-ia.sh
    fixed storage classes and plat-nav name

--- EventEnabledInsurance/build/deploy-ia.sh
+++ EventEnabledInsurance/build/deploy-ia.sh
@@ -27,8 +27,8 @@ CURRENT_DIR=$(dirname $0)
 source $CURRENT_DIR/../../products/bash/utils.sh
 
 NAMESPACE="cp4i"
-BLOCK_STORAGE_CLASS="cp4i-block-performance"
-FILE_STORAGE_CLASS="cp4i-file-performance-gid"
+BLOCK_STORAGE_CLASS="ocs-storagecluster-ceph-rbd"
+FILE_STORAGE_CLASS="ocs-storagecluster-cephfs"
 
 echo "About to process options"
 
@@ -65,8 +65,8 @@ DB_WRITER_FILE='["'${BASE_URL}/EventEnabledInsurance/ACE/BarFiles/DB-WRITER.bar'
 
 PROVIDER_ORG="main-demo"
 CATALOG="${PROVIDER_ORG}-catalog"
-PLATFORM_API="https://$(oc get route -n ${NAMESPACE} ademo-mgmt-platform-api -o jsonpath="{.spec.host}")/"
-CERTIFICATE="$(oc get route -n ${NAMESPACE} ademo-mgmt-platform-api -o json | jq -r .spec.tls.caCertificate)"
+PLATFORM_API="https://$(oc get route -n ${NAMESPACE} cp4i-navigator -o jsonpath="{.spec.host}")/"
+CERTIFICATE="$(oc get route -n ${NAMESPACE} cp4i-navigator -o json | jq -r .spec.tls.caCertificate)"
 CERTIFICATE_NEWLINES_REPLACED=$(echo "${CERTIFICATE}" | awk '{printf "%s\\n", $0}')
 
 
